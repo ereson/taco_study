a25bb59bf9ac8796a38ab74838d0d34222622255
xiongwei <2297896435@qq.com>
2021-12-08 18:32:12 +0800

    add new files

--- src/main/java/com/ereson/toca/web/DesignTacoController.java
+++ src/main/java/com/ereson/toca/web/DesignTacoController.java
@@ -1,13 +1,18 @@
-package com.ereson.toca;
+package com.ereson.toca.web;
 
+import com.ereson.toca.Ingredient;
+import com.ereson.toca.Taco;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
@@ -50,7 +55,11 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco design){
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors){
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
         log.info("Processing design: " + design);
 
         return "redirect:/orders/current";
